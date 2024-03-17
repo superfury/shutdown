b723a0b9f13495198436abf13526be54c737d8bb
superfury <superfury@live.nl>
2024-03-17 18:31:25 +0100

    - Implemented more state support to main.c.

--- main.c
+++ main.c
@@ -23,11 +23,14 @@ void usage() {
          "https://github.com/crgimenes/shutdown\r\n\r\n"
          "Usage:\r\n"
          "\t-h halt require APM\r\n"
-         "\t-r reboot (call BIOS int 19h)\r\n");
+         "\t-s suspend require APM\r\n"
+         "\t-S stand-by require APM\r\n"
+         "\t-R ready require APM\r\n"
+      "\t-r reboot (call BIOS int 19h)\r\n");
 }
 
 int main(int argc, char *argv[]) {
-
+  unsigned int haltstate;
   if (argc == 1) {
     usage();
     return 1;
@@ -42,8 +45,23 @@ int main(int argc, char *argv[]) {
     return 1;
   }
 
-  if (strcmp(argv[1], "-h") == 0) {
-    asm {
+  if (strcmp(argv[1], "-h") == 0) { //Halt?
+      haltstate = 3; //State #3: off!
+  }
+  else if (strcmp(argv[1], "-s") == 0) { //Suspend?
+      haltstate = 2; //State #2: suspend!
+  }
+  else if (strcmp(argv[1], "-S") == 0) { //Stand-by?
+      haltstate = 1; //State #1: stand-by!
+  }
+  else if (strcmp(argv[1], "-R") == 0) { //Ready?
+      haltstate = 0; //State #0: ready!
+  }
+  else {
+      haltstate = 4; //Default: not used!
+  }
+  if (haltstate < 4) { //Valid to set?
+      asm{
       /* connect to APM */
       mov     ax, 5301h
       xor     bx, bx
@@ -58,7 +76,8 @@ int main(int argc, char *argv[]) {
       /* shutdown */
       mov     ax, 5307h
       mov     bx, 0001h
-      mov     cx, 0003h
+      mov     cx,haltstate /* Halt state to set */
+      mov     ch, 0
       int     15h
       hlt
     }
